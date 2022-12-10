22a688f13d930804d461a0c742577fd93a2c1916
kola-web <l15567540858@gmail.com>
2022-12-10 14:08:40 +0800

    flameshot

--- config.def.h
+++ config.def.h
@@ -69,6 +69,8 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          SHCMD("rofi -show drun")  },
 	{ MODKEY,         	            XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,         	  XK_Return, spawn,          SHCMD("flameshot gui") },
+
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
